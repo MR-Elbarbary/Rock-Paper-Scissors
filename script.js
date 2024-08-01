a98e741bbbda8312fd35739279f7cab34c3e0476
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2024-08-01 04:39:20 +0300

    Create js logic for UI
    3 buttons to chose rock, paper or scissors and a 4th button to restart

--- script.js
+++ script.js
@@ -7,16 +7,13 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)]
 }
 
-function getHumanChoice(){
-    while (true) {
-        let humanChoice = prompt("Rock, Paper or Scissors: ");
-        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-        if (choices.includes(humanChoice)) {
-            return humanChoice;
-        } else {
-            console.log("Not an option");
-        }
-    }
+function Restart() {
+    let youScoreP = document.querySelector("#you")
+    youScoreP.innerHTML = 0;
+    let computerScoreP = document.querySelector("#computer")
+    computerScoreP.innerHTML = 0;
+    let message = document.querySelector(".message")
+    message.innerHTML ="chose Rock, paper or scissors";
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -26,14 +23,35 @@ function playRound(humanChoice, computerChoice) {
         'Scissors': { 'Rock': 'Lose', 'Paper': 'Win', 'Scissors': 'Tie' }
     };
 
+    let youScoreP = document.querySelector("#you")
+    let computerScoreP = document.querySelector("#computer")
+    let message = document.querySelector(".message")
     let result = outcomes[humanChoice][computerChoice];
     if (result === 'Win') {
-        humanScore++;
+        youScoreP.innerHTML = ++humanScore;
+        message.innerHTML = `You Win! ${humanChoice} beats ${computerChoice}.`
         console.log(`You Win! ${humanChoice} beats ${computerChoice}.`);
     } else if (result === 'Lose') {
-        computerScore++;
+        computerScoreP.innerHTML = ++computerScore;
+        message.innerHTML = `You Lose! ${computerChoice} beats ${humanChoice}.`
         console.log(`You Lose! ${computerChoice} beats ${humanChoice}.`);
     } else {
+        message.innerHTML = "It's a Tie!"
         console.log("It's a Tie!");
     }
 }
+
+document.addEventListener("DOMContentLoaded", () =>{
+    let options = document.querySelectorAll(".options button")
+    options.forEach(Option => {
+        Option.addEventListener("click", (event) =>{
+            let humanChoice = event.currentTarget.id
+            let computerChoice = getComputerChoice()
+            playRound(humanChoice, computerChoice)
+        })
+    });
+    let restart = document.querySelector("#restart")
+    restart.addEventListener("click",()=>{
+        Restart()
+    })
+})
